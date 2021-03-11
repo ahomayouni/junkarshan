89e18be059ef882c8320014cefcae0064df6ef25
Arash Homayouni <arash.homayouni@hotmail.com>
2021-03-10 22:32:51 -0500

    Initial commit after create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Our website is under construction.</p>
+                <p>Please contact Hassan at (647) 381-4747 if you need assistance.</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
